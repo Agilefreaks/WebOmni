88c99ef40d828e2c8e7cd5ac998b599e74737b7f
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2013-03-30 14:42:00 +0200

    added the welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ WebOmni::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'pages#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
