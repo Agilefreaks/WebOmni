71bb678d44597b61ed33c8ae3f44768811ebe54d
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-08 22:34:32 +0300

    wo-8 fixes spec on api

--- spec/api/resources/activation_tokens_api_spec.rb
+++ spec/api/resources/activation_tokens_api_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe Resources::ActivationTokensAPI do
   describe "PUT 'api/v1/activation_tokens'" do
-    let!(:activation_token) { Fabricate(:activation_token, content: '112233') }
+    let(:activation_token) { Fabricate.build(:activation_token, content: '112233') }
     let!(:user) { Fabricate(:user, activation_tokens: [activation_token]) }
 
     context 'unused activation_token is set in header' do
       it 'is successful' do
+        #noinspection RubyStringKeysInHashInspection
         put 'api/v1/activation_tokens', nil, {'Token' => '112233'}
         expect(response.status).to eql 200
       end
 
       it 'marks the activation_token as used' do
+         #noinspection RubyStringKeysInHashInspection
         put 'api/v1/activation_tokens', nil, {'Token' => '112233'}
         activation_token.reload
         expect(activation_token.used).to eq(true)
