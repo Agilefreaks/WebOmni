85d5268d2b5042fa51b84511df0c70bf885b2686
adi <nistor.adrian@agilefreaks.com>
2013-10-09 17:08:01 +0300

    WO-8 Uses activation_token and device parameters from the body of the request instead of the header

--- app/api/resources/activation_tokens_api.rb
+++ app/api/resources/activation_tokens_api.rb
@@ -1,9 +1,14 @@
 module WebOmni
   class Resources::ActivationTokensAPI < Grape::API
     resources :activation_tokens do
+      desc 'activate a new device using an activation token'
+      params do
+        requires :token, type: String, desc: 'secret activation_token. You can get this from the /whatsmytoken page'
+        requires :device, type: String, desc: 'this is the device type. Possible devices are windows or android'
+      end
       put '/' do
-        user = User.where('activation_tokens.content' => headers['Token']).first
-        activated = ActivationService.new.activate(user.id, headers['Token'], headers['Device'])
+        user = User.where('activation_tokens.content' => params[:token]).first
+        activated = ActivationService.new.activate(user.id, params[:token], params[:device])
         present user, :with => Entities::UserActivateResponseEntity if activated
       end
     end
--- spec/api/resources/activation_tokens_api_spec.rb
+++ spec/api/resources/activation_tokens_api_spec.rb
@@ -5,18 +5,25 @@ describe Resources::ActivationTokensAPI do
     let(:activation_token) { Fabricate.build(:activation_token, content: '112233') }
     let!(:user) { Fabricate(:user, activation_tokens: [activation_token]) }
 
-    context 'unused activation_token is set in header' do
+    context 'unused activation_token is set in the body' do
       it 'is successful' do
         #noinspection RubyStringKeysInHashInspection
-        put 'api/v1/activation_tokens', nil, {'Token' => '112233'}
+        put 'api/v1/activation_tokens', {'token' => '112233', 'device' => 'some_device'}
         expect(response.status).to eql 200
       end
 
       it 'marks the activation_token as used' do
          #noinspection RubyStringKeysInHashInspection
-        put 'api/v1/activation_tokens', nil, {'Token' => '112233'}
+        put 'api/v1/activation_tokens', {'token' => '112233', 'device' => 'some_device' }
         activation_token.reload
-        expect(activation_token.used).to eq(true)
+        expect(activation_token.reload.used).to eq(true)
+      end
+
+      it 'marks the activation_token as used' do
+        #noinspection RubyStringKeysInHashInspection
+        put 'api/v1/activation_tokens', {'token' => '112233', 'device' => 'windows' }
+        activation_token.reload
+        expect(activation_token.reload.type).to eq(:windows)
       end
     end
   end
