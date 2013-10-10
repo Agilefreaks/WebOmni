8d8ea46a7c2bc2eba393a66e31ae629097be542d
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-10 14:44:24 +0300

    wo-8 refactors method
    wo-8 refactors method

--- app/services/activation_service.rb
+++ app/services/activation_service.rb
@@ -13,8 +13,7 @@ class ActivationService
 
   def get_activation_token(user_id)
     user = User.find(user_id)
-    user.activation_tokens.create! if user.activation_tokens.unused.count == 0
 
-    user.activation_tokens.unused.first
+    user.activation_tokens.unused.first_or_create(user: user)
   end
 end
