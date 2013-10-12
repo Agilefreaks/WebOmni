a642a44170d39e07936fc4abf5180a099cd052b6
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-12 11:00:17 +0300

    trying fix for staging env

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require 'rails'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
@@ -34,13 +35,13 @@ module WebOmni
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      :address              => 'smtp.sendgrid.net',
-      :port                 => 587,
-      :domain_name          => 'www.omnipasteapp.com',
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
-      :authentication       => :plain,
-      :enable_starttls_auto => true
-    } 
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
+        :domain_name => 'www.omnipasteapp.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+    }
   end
 end
