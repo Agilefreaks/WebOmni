92d110d17a1d4feb6ed989de9ecffc3cc614933b
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-16 23:36:35 +0300

    using sockets again

--- config/puma/staging.rb
+++ config/puma/staging.rb
@@ -2,13 +2,13 @@ rails_env = 'staging'
 
 threads 0,16
 
-workers 1
+worker 1
 
 pidfile "/var/www/omnipaste_#{rails_env}/shared/sockets/puma.pid"
 state_path "/var/www/omnipaste_#{rails_env}/shared/sockets/puma.state"
 
 stdout_redirect "/var/www/omnipaste_#{rails_env}/shared/log/puma_stdout.log", "/var/www/omnipaste_#{rails_env}/shared/log/puma_stderr.log"
 
-bind 'tcp://0.0.0.0:9292'
+bind "unix:///var/www/omnipaste_#{rails_env}/shared/sockets/puma.sock?umask=0777"
 
 activate_control_app "unix:///var/www/omnipaste_#{rails_env}/shared/sockets/pumactl.sock", { no_token: true }
