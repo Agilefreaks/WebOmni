28945620b87e64634811ad30e555ddd2cc1132b2
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-19 15:23:36 +0300

    adds production recepies

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,16 +1,14 @@
 rails_env = 'production'
 
-environment rails_env
+threads 2,16
 
-daemonize true
-
-threads 0,16
+workers 1
 
 pidfile "/var/www/omnipaste_#{rails_env}/shared/sockets/puma.pid"
 state_path "/var/www/omnipaste_#{rails_env}/shared/sockets/puma.state"
 
-stdout_redirect "/var/www/omnipaste_#{rails_env}/shared/log/puma_stdout.log", "/var/www/omnipaste_#{rails_env}/shared/log/puma_stderr.log", true
+stdout_redirect "/var/www/omnipaste_#{rails_env}/shared/log/puma_stdout.log", "/var/www/omnipaste_#{rails_env}/shared/log/puma_stderr.log"
 
-bind "unix:///var/www/omnipaste_#{rails_env}/shared/sockets/puma.sock"
+bind 'tcp://0.0.0.0:9393'
 
 activate_control_app "unix:///var/www/omnipaste_#{rails_env}/shared/sockets/pumactl.sock", { no_token: true }
