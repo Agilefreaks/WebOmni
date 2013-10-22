c3c31301b96dd29c3c413709fc5fdea4d0dbd641
adi <nistor.adrian@agilefreaks.com>
2013-10-23 00:02:19 +0300

    removed unused css file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,5 @@
 class PagesController < ApplicationController
-  def welcome
-  end
-
-  def pricing
-  end
-
   def free
     redirect_to user_omniauth_authorize_path(:google_oauth2, state: @state)
-  end
-
-  def about
-  end
-
-  def team
-  end
-
-  def contact
   end
 end
