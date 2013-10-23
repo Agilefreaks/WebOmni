6df3497673f6e9f4a336857ff1c332e1bdaebed4
adi <nistor.adrian@agilefreaks.com>
2013-10-23 17:51:53 +0300

    Text fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < ApplicationController
   def startupchile
-    @welcome = 'Hi there Startup Chile folks, glad you dropped by, Omnipaste is a clipboard manager in the cloud and works like your short-term memory.'
+    @welcome = 'Hi there Startup Chile folks, glad you dropped by.'
     @state = :chile
 
     render template: '/pages/welcome'
   end
 
   def soft32
-    @welcome = 'Hi there soft32 visitor, glad you dropped by, Omnipaste is a clipboard manager in the cloud and works like your short-term memory.'
+    @welcome = 'Hi there Soft32 visitor, glad you dropped by.'
     @state = :soft32
 
     render template: '/pages/welcome'
