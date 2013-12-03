37f61a56eaf07ddcf02256450b62169584c7a680
calin <calinoiu.alexandru@agilefreaks.com>
2013-12-03 20:37:12 +0100

    wo-58 adds the call endpoint

--- app/api/resources/devices_api.rb
+++ app/api/resources/devices_api.rb
@@ -6,6 +6,10 @@ module WebOmni
           ActionController::Parameters.new(params).permit(:registrationId)
         end
 
+        def call_permitted_params
+          ActionController::Parameters.new(params).permit(:registrationId, :phone_number)
+        end
+
         def channel
           channel = headers['Channel']
           error!('Unauthorized', 401) if channel.nil?
@@ -27,6 +31,14 @@ module WebOmni
       end
       delete ':registrationId' do
         Unregister.device(channel, permitted_params[:registrationId])
+      end
+
+      desc 'Call the number'
+      params do
+        requires :phone_number, type: String
+      end
+      post '/call' do
+        Call.device(channel, call_permitted_params[:registrationId], call_permitted_params[:phone_number])
       end
     end
   end
