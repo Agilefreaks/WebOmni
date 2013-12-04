9386775796fee671f9dce3b0a9a22fadf4b47a44
calin <calinoiu.alexandru@agilefreaks.com>
2013-12-04 09:55:01 +0100

    removes devices array

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,6 @@ class User
   field :nickname, :type => String
   field :early_adopter, :type => Mongoid::Boolean, :default => -> { User.where(early_adopter: true).count < WebOmni::Application::USER_LIMIT }
   field :image_url, :type => String
-  field :devices, :type => Array
   field :token, :type => String, :default => ->{SecureRandom.uuid}
 
   # relations
