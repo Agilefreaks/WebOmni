edf5fcf6f2e88614f2ccafe14ecedde88b34eb31
calin <calinoiu.alexandru@agilefreaks.com>
2013-12-10 18:05:46 +0200

    wo-61 updates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,43 +2,7 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
-
-  ## Database authenticatable
-  field :email,              :type => String, :default => ''
-  field :encrypted_password, :type => String, :default => ''
-  
-  ## Recoverable
-  field :reset_password_token,   :type => String
-  field :reset_password_sent_at, :type => Time
-
-  ## Rememberable
-  field :remember_created_at, :type => Time
-
-  ## Trackable
-  field :sign_in_count,      :type => Integer, :default => 0
-  field :current_sign_in_at, :type => Time
-  field :last_sign_in_at,    :type => Time
-  field :current_sign_in_ip, :type => String
-  field :last_sign_in_ip,    :type => String
-
-  ## Confirmable
-  # field :confirmation_token,   :type => String
-  # field :confirmed_at,         :type => Time
-  # field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
-
-  ## Token authenticatable
-  # field :authentication_token, :type => String
+  include Concerns::UserDevise
 
   # fields
   field :first_name, :type => String
@@ -67,6 +31,6 @@ class User
   end
 
   def self.find_by_provider(email, provider)
-    User.where('providers.email' => email, 'providers.name' => provider).first
+    User.find_by('providers.email' => email, 'providers.name' => provider)
   end
 end
