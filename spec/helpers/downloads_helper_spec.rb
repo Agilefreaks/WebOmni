2994e02a58b119f050f8fc0464aed3061aa7f125
adi <nistor.adrian@agilefreaks.com>
2013-12-11 17:00:27 +0200

    removed spec for winomni_url

--- spec/helpers/downloads_helper_spec.rb
+++ spec/helpers/downloads_helper_spec.rb
@@ -1,22 +1,6 @@
 require 'spec_helper'
 
 describe DownloadsHelper do
-  describe 'winomni_url' do
-    subject { helper.winomni_url }
-
-    context 'when Rails.env is staging' do
-      before { Rails.stub_chain(:env, :staging?).and_return(true) }
-
-      it { should == 'http://cdn.omnipasteapp.com/staging/win/Omnipaste-staging.application' }
-    end
-
-    context 'when Rails.env is production' do
-      before { Rails.stub_chain(:env, :staging?).and_return(false) }
-
-      it { should == 'http://cdn.omnipasteapp.com/production/win/Omnipaste.application' }
-    end
-  end
-
   describe 'android url' do
     subject { helper.android_url }
 
