edb9b5de7754c2f7bfcbfbb5162346d8ef1621bc
adi <nistor.adrian@agilefreaks.com>
2013-12-11 17:03:32 +0200

    Renamed the download file name

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -7,6 +7,6 @@ class DownloadsController < ApplicationController
   def windows
     @token = ActivationService.new.get_activation_token(current_user.id).content
     data = open(WINDOWS_CLIENT_DOWNLOAD_LINK)
-    send_data data.read, :filename => "omnipaste-setup#{@token.content}.exe"
+    send_data data.read, :filename => "OmnipasteSetup-#{@token.content}.exe"
   end
 end
