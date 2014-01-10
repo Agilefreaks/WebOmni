067ea4408196e9252f4a9c53c0593955624c96ea
calin <calinoiu.alexandru@agilefreaks.com>
2014-01-10 11:53:26 +0200

    forces date format to iso

--- app/api/entities/registered_device_entity.rb
+++ app/api/entities/registered_device_entity.rb
@@ -1,5 +1,12 @@
 module Entities
   class RegisteredDeviceEntity < Grape::Entity
-    expose :identifier, :name, :registration_id, :updated_at, :created_at
+    format_with(:iso_timestamp) { |dt| dt.iso8601 }
+
+    expose :identifier, :name, :registration_id
+
+    with_options(format_with: :iso_timestamp) do
+      expose :created_at
+      expose :updated_at
+    end
   end
 end
