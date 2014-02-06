b6e3668212e91b929eb3fbd86f291c4914618196
adi <nistor.adrian@agilefreaks.com>
2014-02-06 19:42:04 +0200

    Adds provider field on the registered device. This specifies what notification provider is used by the device. For the moment, it can be :gcm or :omni

--- spec/models/registered_device_spec.rb
+++ spec/models/registered_device_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe RegisteredDevice do
   it { should validate_presence_of :identifier }
 
+  it {should ensure_inclusion_of(:provider).in_array [:gcm, :omni] }
+
   it { should be_embedded_in :user }
 
   describe 'active' do
