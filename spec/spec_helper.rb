99408109479fbce6b58b8b64dc1abe79592c3f40
calin <calinoiu.alexandru@agilefreaks.com>
2014-02-14 13:58:09 +0200

    adds RAILS_ENV to rspec_helepr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+ENV['RAILS_ENV'] ||= 'test' 
+
 require 'simplecov'
 SimpleCov.start 'rails'
 
@@ -7,8 +9,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'email_spec'
 require 'rspec/rails'
 require 'rspec-spies'
-
-ENV["RAILS_ENV"] = 'test' 
 
 # Require all of the RSpec Support libraries
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
