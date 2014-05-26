6acdff3565333fadb7abb58a52db1419b35b2aee
calin <calinoiu.alexandru@agilefreaks.com>
2014-05-26 17:27:01 +0300

    [wo-112] updates smpt server

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,6 +2,6 @@ ActionMailer::Base.smtp_settings = {
     :user_name => 'AKIAJ22XOZ32YQ6MEOSA',
     :password => 'AiqsPPiyyddwhsPA3WAn4sfGrFpROndV7oQfk1PWwPOz',
     :domain => 'omnipasteapp.com',
-    :address => 'email-smtp.us-east-1.amazonaws.com',
+    :address => 'email-smtp.eu-west-1.amazonaws.com',
     :port => 25
 }
