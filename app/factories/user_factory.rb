abcaa0872798ebe5e286c3394ea1a280e5bc6167
calin <calinoiu.alexandru@agilefreaks.com>
2014-05-26 15:38:55 +0300

    [wo-104] updates the access token on user login

--- app/factories/user_factory.rb
+++ app/factories/user_factory.rb
@@ -3,10 +3,10 @@ class UserFactory
 
   def self.from_social(auth, user)
     factory = UserFactory.instance
-    factory.create_from_social(auth)
+    factory.create_or_update_from_social(auth, user)
   end
 
-  def create_from_social(auth)
+  def create_or_update_from_social(auth, user)
     match = auth.to_s.match(/image=\"(.*?)\"/)
 
     api_user = OmniApi::User.where(email: auth.info.email).first ||
@@ -15,12 +15,16 @@ class UserFactory
                           :email => auth.info.email)
     api_user.save
 
-    user = User.create(first_name: auth.info.first_name,
-                       last_name: auth.info.last_name,
-                       email: auth.info.email,
-                       password: Devise.friendly_token[0, 20],
-                       image_url: match ? match[1] : nil,
-                       access_token: api_user.access_token)
+    params = {
+        first_name: auth.info.first_name,
+        last_name: auth.info.last_name,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        image_url: match ? match[1] : nil,
+        access_token: api_user.access_token
+    }
+
+    user ? user.update(params) : user.create(params)
 
     # NotificationsMailer.welcome(user.id).deliver
 
