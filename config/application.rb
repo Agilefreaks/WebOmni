dd1f7a4858d10536614f8ef645124aede3fa5547
calin <calinoiu.alexandru@agilefreaks.com>
2014-06-09 16:28:28 +0200

    [wo-115]  adds animation files to the precompilation

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,8 @@ module WebOmni
 
     config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.precompile += %w( .svg .eot .woff .ttf
+                                    animation-1_edge.js animation-1_edgeActions.js animation-1_edgePreload.js edge.3.0.0.min )
 
     config.paths.add 'app/api', glob: "**/*.rb"
     config.autoload_paths += Dir["#{Rails.root}/app/api/*",
