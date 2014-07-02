f77aaed0710f5c3631bf147de8ad0579c20d4557
calin <calinoiu.alexandru@agilefreaks.com>
2014-07-02 10:20:59 +0300

    [wo-137] improves animation load time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-2.1.1.min
 //= require jquery_ujs
 //= require jquery.easing
 //= require jquery.slimscroll
