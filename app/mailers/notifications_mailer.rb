535430f7ff6c015e890115da5a5ac9d75d517439
calin <calinoiu.alexandru@agilefreaks.com>
2014-08-13 13:48:37 +0300

    wo-175 removes unnecessary emailing

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -7,13 +7,4 @@ class NotificationsMailer < BaseMailer
              to: @user.email
          })
   end
-
-  def install_instructions_android(user_id)
-    @user = User.find(user_id)
-    mail({
-             subject: 'Omnipaste installation instructions',
-             content_type: 'text/html',
-             to: @user.email
-         })
-  end
 end
