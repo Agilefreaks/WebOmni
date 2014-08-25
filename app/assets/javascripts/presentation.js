1f8085a973434dd55e4130c560e5e490231b121f
calin <calinoiu.alexandru@agilefreaks.com>
2014-08-25 20:41:23 +0300

    merged latest changes

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -27,6 +27,344 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   document.getElementsByTagName('head')[0].appendChild(msViewportStyle);
 }
 
+// Global
+var $htmlAction = $('html body'),
+//  Scroll vars
+  lastScrollTop = 0,
+  poolingDelay = 250,
+  $menuContent = $('.js-header-nav-content'),
+  menuDetachedClass = 'header--detached',
+  menuVisibleClass = 'header--visible',
+  menuHiddenClass = 'header--hidden',
+  scrollDelay = 750,
+  scrollDelta	= 5,
+  scrollEvent = false,
+  detachDelay	= 200,
+// Dropdown vars
+  $bodyTrigger = $('.js-body-action'),
+  bodyClass = 'omni-video--action',
+  $dropdown = $('.js-dropdown'),
+  $dropdownAction = $('.js-toggle-dropdown'),
+  dropdownClass = 'dropdown--action',
+  dropdownActiveClass	= 'dropdown--open',
+  dropdownBodyClass = 'dropdown--action',
+// Toggle vars
+  $sectionAction = $('.js-goto-section'),
+// Video vars
+  $videoPlayer = $('.js-omni-video-player'),
+  $videoPlayerObject = $('.js-omni-video-player').get(0),
+  $videoContainer = $('.js-omni-video'),
+  $videoSeek = $('.js-omni-video-seek'),
+  $videoSeekObject = $('.js-omni-video-seek').get(0),
+  $videoToggle = $('.js-omni-video-toggle'),
+  videoActiveClass = 'omni-video--active',
+  videoBodyClass = 'omni-video--action',
+  videoTimeout = 1000,
+// AuthCode	vars
+  authCode = false,
+  $authCodeAction = $('.js-get-auth-code'),
+  authCodeURL = '#',
+  authCodeInvisibleClass = 'button--invisible',
+// Contact form vars
+  $contactForm = $('.js-contact-form'),
+  $contactFormNotice = $('.js-contact-notice'),
+  $contactFormFields = $('.js-contact-form input, .js-contact-form textarea'),
+  $contactFormContainer = $('.js-contact-form-content');
+
+var omnipaste = {
+
+  init: function() {
+
+    // Open in new window links with rel=external code
+    omnipaste.externalLink();
+
+    // Prevent default action on # (hash) links
+    omnipaste.preventLink();
+
+    // Menu hide / show on scroll
+    omnipaste.scroll();
+
+    // Scroll to section
+    omnipaste.scrollToSection();
+
+    // Toggle dropdown
+    omnipaste.sectionToggle();
+
+    // Video player
+    omnipaste.video();
+
+    // Generate authcode
+    omnipaste.authCode();
+
+    // Contact form
+    omnipaste.contact();
+  },
+
+  externalLink: function(obj) { $('a[rel="external"]').attr('target','_blank'); },
+
+  preventLink: function(obj) { $('a[href="#"]').on('click', function(e) { e.preventDefault(); }); },
+
+  scroll: function(obj) {
+    // Check for scroll function
+    $(window).scroll(function(e) { scrollEvent = true; });
+
+    // Do scroll polling at 250ms intervals
+    setInterval (function() {
+      if (scrollEvent) {
+        hasScrolled();
+        scrollEvent = false;
+      }
+    }, poolingDelay);
+
+    // If scrolled do things
+    function hasScrolled() {
+      var navbarHeight = $menuContent.outerHeight(),
+        currentPosition = $(this).scrollTop();
+
+      // Make sure the scroll is more than delta
+      if (Math.abs(lastScrollTop - currentPosition) <= scrollDelta) { return; }
+
+      if ( currentPosition > navbarHeight ) {
+        // Needs to fire after menuHidden to prevent hide animation
+        setTimeout(function() {
+          // Detach menu
+          $menuContent.addClass(menuDetachedClass);
+        }, detachDelay);
+      } else {
+        // Attach menu
+        $menuContent.removeClass(menuDetachedClass);
+      }
+      // Hide on scroll down
+      if (currentPosition > lastScrollTop && currentPosition > navbarHeight) {
+        // Scroll down
+        $menuContent.removeClass(menuVisibleClass).addClass(menuHiddenClass);
+
+      } else {
+        // Scroll Up
+        if (currentPosition + $(window).height() < $(document).height()) {
+          $menuContent.removeClass(menuHiddenClass).addClass(menuVisibleClass);
+        }
+      }
+      lastScrollTop = currentPosition;
+    }
+  },
+  // Scroll to top function
+  scrollToTop: function ( scrollDelay ) {
+    // Set defaults
+    scrollDelay = scrollDelay || 750;
+
+    $htmlAction.animate({ scrollTop: 0 }, scrollDelay);
+  },
+
+  // Scroll to ID function
+  scrollToID: function (id, scrollDelay, scrollOffset) {
+    // Set defaults
+    scrollDelay = scrollDelay || 750;
+    scrollOffset = scrollOffset || 0;
+
+    var targetOffset = $('#' + id).offset().top - scrollOffset;
+
+    $htmlAction.animate({ scrollTop: targetOffset }, scrollDelay);
+
+  },
+  // Scroll to section
+  scrollToSection: function () {
+
+    $sectionAction.on('click', function(e) {
+      var sectionID = $(this).attr('data-id');
+
+      // Scroll
+      omnipaste.scrollToID(sectionID);
+      //Prevent default action
+      e.preventDefault();
+    });
+  },
+
+  // Toggle section
+  sectionToggle: function () {
+    // Section Toggle
+    $dropdownAction.on('click', function (e) {
+      var $dropdownID = $('#' + $(this).attr('data-id'));
+
+      $dropdownID.toggleClass(dropdownActiveClass);
+      $bodyTrigger.toggleClass(dropdownBodyClass);
+
+      // Scroll to top
+      omnipaste.scrollToTop();
+      // Prevent default action
+      e.preventDefault();
+    });
+  },
+
+  // Video functions
+  video: function () {
+    // Video Play
+    $videoToggle.on('click', function (e) {
+      var videoAction	= $(this).attr("data-action");
+
+      // Make sure dropdown is closed
+      $dropdown.removeClass(dropdownActiveClass);
+      $bodyTrigger.removeClass(dropdownClass);
+
+      // Overlay video
+      $videoContainer.toggleClass(videoActiveClass);
+      $bodyTrigger.toggleClass(videoBodyClass);
+
+      if (videoAction == 'play') {
+        $videoPlayerObject.play();
+      } else {
+        $videoPlayerObject.pause();
+        $videoPlayerObject.currentTime = 0;
+        $videoPlayerObject.load();
+      }
+      // Scroll to top
+      omnipaste.scrollToTop();
+      // Prevent default action
+      e.preventDefault();
+    });
+
+    // Video play / pause toggle
+    $videoPlayer.on('click', function () {
+      if ($videoPlayerObject.paused === false) {
+        $videoPlayerObject.pause();
+      } else {
+        $videoPlayerObject.play();
+      }
+    });
+
+    // Close video on end
+    $videoPlayer.on('ended', function() {
+
+      timer = setTimeout( function() {
+
+        $videoPlayerObject.currentTime = 0;
+        $videoPlayerObject.load();
+        $videoContainer.toggleClass(videoActiveClass);
+        $bodyTrigger.toggleClass(videoBodyClass);
+
+      }, videoTimeout);
+    });
+
+    // Full screen on double click
+    $videoPlayer.on('dblclick', function() {
+      if ($videoPlayerObject.requestFullscreen) {
+        $videoPlayerObject.requestFullscreen();
+
+      } else if ($videoPlayerObject.mozRequestFullScreen) {
+        $videoPlayerObject.mozRequestFullScreen(); // Firefox
+
+      } else if ($videoPlayerObject.webkitRequestFullscreen) {
+        $videoPlayerObject.webkitRequestFullscreen(); // Chrome and Safari
+      }
+    });
+
+    // Hook the slider with the video
+    $videoSeek.on('change', function() {
+      // Calculate the new time
+      var time = $videoPlayerObject.duration * ($videoSeekObject.value / 100);
+      // Update the video time
+      $videoPlayerObject.currentTime = time;
+    });
+
+    // Update the slider with the video progress
+    $videoPlayer.on('timeupdate', function() {
+      var value;
+
+      // Calculate the slider value
+      value = (100 / $videoPlayerObject.duration) * $videoPlayerObject.currentTime;
+      // Update the slider value
+      $videoSeekObject.value = value;
+    });
+
+    // Pause the video when the slider handle is being dragged
+    $videoSeek.on("mousedown", function() { $videoPlayerObject.pause(); });
+
+    // Play the video when the slider handle is dropped
+    $videoSeek.on("mouseup", function() { $videoPlayerObject.play(); });
+
+  },
+
+  // Get Auth Code
+  authCode: function() {
+
+    $authCodeAction.on('click', function (e) {
+      var token = $(this).attr('data-token');
+
+      if (authCode === false) {
+        $.ajax({
+          url: authCodeURL,
+          data: token,
+          type: 'get',
+          success: function(msg) {
+            msg = "Gbqw0Vpr86RZ"; // Dev: Fixed message for display purposes - REMOVE THIS LINE!!!
+            $authCodeAction.addClass(authCodeInvisibleClass);
+            timer1 = setTimeout( function() {
+              $authCodeAction.html(msg);
+              $authCodeAction.removeClass(authCodeInvisibleClass);
+            },1000);
+          }
+        });
+        authCode = true;
+      }
+    });
+  },
+  // Contact form
+  contact: function() {
+    // Validate Contact Form
+    $contactForm.h5Validate();
+
+    // Process Contact Form
+    $contactForm.submit(function(e) {
+      var result = $contactForm.h5Validate('allValid'),
+        data,
+        url;
+
+      if ( result === true ) {
+        // Serialize contact data
+        data = $(this).serialize();
+        // Get URL from action
+        url = $(this).attr('action');
+
+        // Send request
+        $.ajax({
+          url: url,
+          data: data,
+          type: 'post',
+          success: function(msg) {
+
+            // Place error message in notice
+            //$contactFormNotice.html(msg);
+
+            // Push Google Analytics event
+            //_gaq.push(['_trackEvent', 'Contact', 'Contact request', 'Contact sent!']);
+          }
+        });
+
+        // Fade out & display message
+        $contactFormContainer.fadeOut(800);
+
+        timer1 = setTimeout(function() {
+          $contactFormNotice.fadeIn(1000);
+          fields.val('');
+        },1000);
+
+        // Fade out & reset form
+        timer2 = setTimeout(function() {
+          $contactFormNotice.fadeOut(800);
+        },4500);
+
+        // Fade in form
+        timer3 = setTimeout( function() {
+          $contactFormContainer.fadeIn(1000);
+        },5500);
+      }
+      // Prevent actual form submit
+      e.preventDefault();
+    });
+  }
+};
+
+
 // !Document ready (loaded)
 // --------------------------------------------------------------
 jQuery(document).ready(function($) {
@@ -45,265 +383,8 @@ jQuery(document).ready(function($) {
    if (isChrome()) { $('html').addClass(' chrome'); }
    */
 
-  // Open in new window links with rel=external code
-  $('a[rel="external"]').attr('target','_blank');
-
-  // Prevent default action on # (hash) links
-  $('a[href="#"]').click( function(e) { e.preventDefault(); });
-
-  // Initialize scroll vars
-  var didScroll;
-  var lastScrollTop	= 0;
-  var poolingDelay	= 250;
-
-  // Check for scroll function
-  $(window).scroll(function(e) { didScroll = true; });
-
-  // Do scroll polling at 250ms intervals
-  setInterval (function() {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, poolingDelay);
-
-  // If we have scroll detected do stuff
-  function hasScrolled() {
-    var menuContent		= $('.js-header-nav-content');
-    var menuDetached	= 'header--detached';
-    var menuVisible		= 'header--visible';
-    var menuHidden		= 'header--hidden';
-    var navbarHeight	= menuContent.outerHeight();
-    var currentPosition	= $(this).scrollTop();
-    var delta			= 5;
-    var detachDelay		= 200;
-
-    // Make sure the scroll is more than delta
-    if (Math.abs(lastScrollTop - currentPosition) <= delta) { return; }
-
-    if ( currentPosition > navbarHeight ) {
-      // Needs to fire after menuHidden to prevent hide animation
-      setTimeout(function() {
-        // Detach menu
-        menuContent.addClass(menuDetached);
-      }, detachDelay);
-    } else {
-      // Attach menu
-      menuContent.removeClass(menuDetached);
-    }
-    // Hide on scroll down
-    if (currentPosition > lastScrollTop && currentPosition > navbarHeight) {
-      // Scroll down
-      menuContent.removeClass(menuVisible).addClass(menuHidden);
-
-    } else {
-      // Scroll Up
-      if (currentPosition + $(window).height() < $(document).height()) {
-        menuContent.removeClass(menuHidden).addClass(menuVisible);
-      }
-    }
-    lastScrollTop = currentPosition;
-  }
-
-  // Scroll to top function
-  function scrollToTop (speed) { $('html, body').animate({ scrollTop: 0 }, speed); }
-
-  // Scroll to ID function
-  function scrollToID (id, speed){
-    var scrollOffset	= 0;
-    var targetOffset	= $('#' + id).offset().top - scrollOffset;
-
-    $('html,body').animate({ scrollTop: targetOffset }, speed);
-  }
-
-  // Scroll to section
-  $('.js-goto-section').on('click', function(e) {
-    var sectionID	= $(this).attr('data-id');
-    var scrollDelay	= 750;
-
-    // Scroll
-    scrollToID(sectionID, scrollDelay);
-    //Prevent default action
-    e.preventDefault();
-  });
-
-  // Section Toggle
-  $('.js-toggle-dropdown').on('click', function (e) {
-    var bodyTrigger		= $('.js-body-action');
-    var bodyClass		= 'dropdown--action';
-    var dropdownActive	= 'dropdown--open';
-    var dropdownID		= $('#' + $(this).attr('data-id'));
-    var scrollDelay		= 750;
-
-    dropdownID.toggleClass(dropdownActive);
-    bodyTrigger.toggleClass(bodyClass);
-
-    // Scroll to top
-    scrollToTop(scrollDelay);
-    // Prevent default action
-    e.preventDefault();
-  });
-
-  //Video Play
-  $('.js-omni-video').on('click', function (e) {
-    var bodyTrigger		= $('.js-body-action');
-    var bodyClass		= 'omni-video--action';
-    var dropdownClass	= 'dropdown--action';
-    var globalDropdown	= $('.dropdown');
-    var dropdownActive	= 'dropdown--open';
-    var video			= $('.js-omni-video-player').get(0);
-    var videoActive		= 'omni-video--active';
-    var videoID			= $('#' + $(this).attr('data-id'));
-    var videoAction		= $(this).attr("data-action");
-    var scrollDelay		= 750;
-
-    globalDropdown.removeClass(dropdownActive);
-    bodyTrigger.removeClass(dropdownClass);
-
-    videoID.toggleClass(videoActive);
-    bodyTrigger.toggleClass(bodyClass);
-
-    if (videoAction == 'play') {
-      video.play();
-    } else {
-      video.pause();
-      video.currentTime = 0;
-      video.load();
-    }
-    // Scroll to top
-    scrollToTop(scrollDelay);
-    // Prevent default action
-    e.preventDefault();
-  });
-
-  // Video play / pause toggle
-  var $omni_video_player = $('.js-omni-video-player');
-  $omni_video_player.on('click', function () {
-    var video = $('.js-omni-video-player').get(0);
-
-    if (video.paused === false) {
-      video.pause();
-    } else {
-      video.play();
-    }
-  });
-
-  // Close video on end
-  $omni_video_player.on('ended', function() {
-    var video			= $('.js-omni-video-player').get(0);
-    var videoContainer	= $('#omni-video');
-    var videoActive		= 'omni-video--active';
-    var bodyTrigger		= $('.js-body-action');
-    var bodyClass		= 'omni-video--action';
-    var videoTimeout	= 1000;
-
-    timer = setTimeout( function() {
-
-      video.currentTime = 0;
-      video.load();
-      videoContainer.toggleClass(videoActive);
-      bodyTrigger.toggleClass(bodyClass);
-
-    }, videoTimeout);
-  });
-
-  // Full screen on double click
-  $omni_video_player.on('dblclick', function() {
-    var video = $('.js-omni-video-player').get(0);
-
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-
-    } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen(); // Firefox
-
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen(); // Chrome and Safari
-    }
-  });
-
-  // Update the slider with the video progress
-  $omni_video_player.on('timeupdate', function() {
-    var seekBar	= $('.js-omni-video-seek').get(0);
-    var video	= $('.js-omni-video-player').get(0);
-    var value;
-
-    // Calculate the slider value
-    value = (100 / video.duration) * video.currentTime;
-    // Update the slider value
-    seekBar.value = value;
-  });
-
-  // Hook the slider with the video
-  var $js_omni_video_seek = $('.js-omni-video-seek');
-  $js_omni_video_seek.on('change', function() {
-    var seekBar	= $('.js-omni-video-seek').get(0);
-    var video	= $('.js-omni-video-player').get(0);
-
-    // Calculate the new time
-    video.currentTime = video.duration * (seekBar.value / 100);
-  });
-
-  // Pause the video when the slider handle is being dragged
-  $js_omni_video_seek.on("mousedown", function() {
-    var video = $('.js-omni-video-player').get(0);
-
-    video.pause();
-  });
-  // Play the video when the slider handle is dropped
-  $js_omni_video_seek.on("mouseup", function() {
-    var video = $('.js-omni-video-player').get(0);
-
-    video.play();
-  });
-
-  // Validate Contact Form
-  var $js_contact_form = $('.js-contact-form');
-  $js_contact_form.h5Validate();
-
-  // Process Contact Form
-  $js_contact_form.submit(function(e) {
-    var result		= $('.js-contact-form').h5Validate('allValid');
-    var notice		= $('.js-contact-notice');
-    var fields		= $('.js-contact-form input, .js-contact-form textarea');
-    var container	= $('.js-contact-form-content');
-    var data;
-    var url;
-
-    if ( result === true ) {
-      // Serialize contact data
-      data = $(this).serialize();
-      // Get URL from action
-      url = $(this).attr('action');
-
-      // Send request
-      $.ajax({
-        url: url,
-        data: data,
-        type: 'post'
-      });
-
-      // Fade out & display message
-      container.fadeOut(800);
-      var timer1 = setTimeout(function() {
-        notice.fadeIn(1000);
-        fields.val('');
-      },1000);
-
-      // Fade out & reset form
-      var timer2 = setTimeout(function() {
-        notice.fadeOut(800);
-      },4500);
-
-      // Fade in form
-      var timer3 = setTimeout( function() {
-        container.fadeIn(1000);
-      },5500);
-    }
-    // Prevent actual form submit
-    e.preventDefault();
-  });
-
+  // Init scripts
+  omnipaste.init();
 
 // !---- End Document Ready Function ----
 });
@@ -313,6 +394,7 @@ jQuery(document).ready(function($) {
 // --------------------------------------------------------------
 
 jQuery(window).load(function($) {
+
 
 // !---- End Document Load Function ----
 });
