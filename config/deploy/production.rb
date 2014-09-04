ede7afed64ab075ea097c6996a0df53eb36786fd
calin <calinoiu.alexandru@agilefreaks.com>
2014-09-04 09:19:19 +0300

    adds rails_env in the deploy files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 set :deploy_to, '/var/www/webomni'
 set :branch, 'production'
 
+set :rails_env, 'production'
+
 role :app, %w(deploy@5.10.81.83)
 role :web, %w(deploy@5.10.81.83)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,7 @@
 set :deploy_to, '/var/www/webomni'
 set :branch, 'staging'
 
+set :rails_env, 'staging'
+
 role :app, %w(deploy@5.10.81.82)
 role :web, %w(deploy@5.10.81.82)
