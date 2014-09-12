6cae706b3f0d5478596725f4469652be4fccd764
calin <calinoiu.alexandru@agilefreaks.com>
2014-09-12 12:10:48 +0300

    [wo-251] embeds images into the email

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,3 +1,15 @@
 class BaseMailer < ActionMailer::Base
   default from: 'Omnipaste <team@omnipasteapp.com>'
+
+  before_action :populate_inline_attachments
+
+  private
+  def populate_inline_attachments
+    attachments.inline['background.jpg'] = File.read(WebOmni::Application.assets.find_asset('mailers/background.jpg').pathname)
+    attachments.inline['logo.png'] = File.read(WebOmni::Application.assets.find_asset('mailers/logo.png').pathname)
+    attachments.inline['icon_linkedin.png'] = File.read(WebOmni::Application.assets.find_asset('mailers/icon-linkedin.png').pathname)
+    attachments.inline['icon_facebook.png'] = File.read(WebOmni::Application.assets.find_asset('mailers/icon-facebook.png').pathname)
+    attachments.inline['icon_googleplus.png'] = File.read(WebOmni::Application.assets.find_asset('mailers/icon-googleplus.png').pathname)
+    attachments.inline['icon_twitter.png'] = File.read(WebOmni::Application.assets.find_asset('mailers/icon-twitter.png').pathname)
+  end
 end
