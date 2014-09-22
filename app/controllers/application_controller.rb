8d61805efa3f5baa96193c55bcab60a8e0439ea3
calin <calinoiu.alexandru@agilefreaks.com>
2014-09-22 15:34:53 +0300

    keeps params locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = extract_locale_from_accept_language_header
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header
   end
 
   private
