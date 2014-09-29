ba89726934652dcc973aade37b69ba9a36de3c69
calin <calinoiu.alexandru@agilefreaks.com>
2014-09-29 22:11:25 +0300

    adds new staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,4 @@ set :branch, 'staging'
 set :rails_env, 'staging'
 
 role :app, %w(deploy@webstaging01.omnipasteapp.com)
-role :web, %w(deploy@webstaging02.omnipasteapp.com)
+role :web, %w(deploy@webstaging01.omnipasteapp.com)
