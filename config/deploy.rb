e17a5012f9372ce07e62e956e6915442860bd92b
calin <calinoiu.alexandru@agilefreaks.com>
2014-12-03 13:30:51 +0200

    updates to ruby-2.1.5

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.2.1'
 set :application, 'omnipaste'
 set :repo_url, 'git@github.com:Agilefreaks/WebOmni.git'
 
-set :rvm_ruby_version, 'ruby-2.1.3@webomni'
+set :rvm_ruby_version, 'ruby-2.1.5@webomni'
 
 set :assets_roles, [:web, :app]
 
