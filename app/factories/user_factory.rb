be198b5db95e6ad0497d44076d726c98172fce6c
calin <calinoiu.alexandru@agilefreaks.com>
2014-12-12 17:52:13 +0200

    [oa-104] passes image url over to the api

--- app/factories/user_factory.rb
+++ app/factories/user_factory.rb
@@ -7,14 +7,13 @@ class UserFactory
   end
 
   def create_or_update_from_social(auth, user)
-    match = auth.to_s.match(/image="(.*?)"/)
-
     Track.sign_up(auth.info.email)
 
     api_user = OmniApi::User.where(email: auth.info.email).first ||
         OmniApi::User.new(first_name: auth.info.first_name,
                           last_name: auth.info.last_name,
-                          email: auth.info.email)
+                          email: auth.info.email,
+                          image_url: getImageUrl(auth))
     api_user.save
 
     params = {
@@ -22,7 +21,7 @@ class UserFactory
         last_name: auth.info.last_name,
         email: auth.info.email,
         password: Devise.friendly_token[0, 20],
-        image_url: match ? match[1] : nil,
+        image_url: getImageUrl(auth),
         access_token: api_user.access_token,
         mixpanel_distinct_id: auth.distinct_id,
         remote_ip: auth.remote_ip
@@ -35,5 +34,10 @@ class UserFactory
     end
 
     user
+  end
+
+  def getImageUrl(auth)
+    match = auth.to_s.match(/image="(.*?)"/)
+    match ? match[1] : nil
   end
 end
