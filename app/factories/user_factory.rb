2d88e0e4308c9ab85215e17b03f115f4e4ae5cdd
Calin <calinoiu.alexandru@agilefreaks.com>
2014-12-18 20:32:59 +0200

    does api_user creation afterwards

--- app/factories/user_factory.rb
+++ app/factories/user_factory.rb
@@ -9,22 +9,15 @@ class UserFactory
   def create_or_update_from_social(auth, user)
     Track.sign_up(auth.info.email)
 
-    api_user = OmniApi::User.where(email: auth.info.email).first ||
-        OmniApi::User.new(first_name: auth.info.first_name,
-                          last_name: auth.info.last_name,
-                          email: auth.info.email,
-                          image_url: get_image_url(auth))
-    api_user.save
-
     params = {
-        first_name: auth.info.first_name,
-        last_name: auth.info.last_name,
-        email: auth.info.email,
-        password: Devise.friendly_token[0, 20],
-        image_url: get_image_url(auth),
-        access_token: api_user.access_token,
-        mixpanel_distinct_id: auth.distinct_id,
-        remote_ip: auth.remote_ip
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      email: auth.info.email,
+      password: Devise.friendly_token[0, 20],
+      image_url: get_image_url(auth),
+      access_token: api_user.access_token,
+      mixpanel_distinct_id: auth.distinct_id,
+      remote_ip: auth.remote_ip
     }
 
     if user
@@ -33,6 +26,13 @@ class UserFactory
       user = User.create(params)
       Track.alias(params[:email], auth.distinct_id, params)
     end
+
+    api_user = OmniApi::User.where(email: user.email).first ||
+      OmniApi::User.new(first_name: user.first_name,
+                        last_name: user.last_name,
+                        email: user.email,
+                        image_url: user.image_url)
+    api_user.save
 
     user
   end
