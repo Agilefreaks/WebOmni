6ef1da5cac73cc4eb980bf4a91cb8d59f2f7e598
Calin <calinoiu.alexandru@agilefreaks.com>
2015-01-28 09:45:27 +0200

    updates cap to ruby 2.2.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.3.5'
 set :application, 'omnipaste'
 set :repo_url, 'git@github.com:Agilefreaks/WebOmni.git'
 
-set :rvm_ruby_version, 'ruby-2.1.5@webomni'
+set :rvm_ruby_version, 'ruby-2.2.0@webomni'
 
 set :assets_roles, [:web, :app]
 
