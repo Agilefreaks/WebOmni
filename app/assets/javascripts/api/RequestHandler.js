5acac808a8b0324773c5da47131293ba451df8af
Cristi Badila <cristi.badila@gmail.com>
2015-04-21 19:38:25 +0300

    [wo-346]  updates the RequestHandler so as to send the current user access token when a valid request is received

--- app/assets/javascripts/api/RequestHandler.js
+++ app/assets/javascripts/api/RequestHandler.js
@@ -1,10 +1,35 @@
-define('api/RequestHandler', [], function() {
-  var RequestHandler = function() {
+define('api/RequestHandler', ['lodash', './DataStore'], function (_, DataStore) {
+  var RequestHandler = function () {
   };
 
-  RequestHandler.prototype.handle = function() {
-    console.log('received a new request');
-  };
+  function parseRequestOptions(messageData) {
+    var options;
+    try {
+      options = JSON.parse(messageData)
+    } catch (exception) {
+    }
+
+    return options;
+  }
+
+  _.extend(RequestHandler.prototype, {
+    isAuthorized: function (message) {
+      return message.origin == DataStore.apiClientUrl;
+    },
+
+    handle: function (message) {
+      if (this.isAuthorized(message)) {
+        var requestOptions = parseRequestOptions(message.data);
+        if (requestOptions) {
+          switch (requestOptions.action) {
+            case 'getUserAccessToken':
+              message.source.postMessage(DataStore.userAccessToken);
+              break;
+          }
+        }
+      }
+    }
+  });
 
   return RequestHandler;
 });
---
+++ spec/javascript/api/RequestHandler_spec.js
@@ -0,0 +1,89 @@
+define(['api/RequestHandler', 'api/DataStore'], function (RequestHandler, DataStore) {
+  describe('api/RequestHandler', function () {
+    var instance, subject;
+    beforeEach(function () {
+      instance = new RequestHandler();
+      subject = function () {
+        return instance;
+      }
+    });
+
+    describe('isAuthorized', function () {
+      var message;
+
+      beforeEach(function () {
+        message = {};
+
+        subject = function () {
+          return instance.isAuthorized(message);
+        }
+      });
+
+      describe('the message has an origin different then the api client url', function () {
+        beforeEach(function () {
+          message.origin = 'someUrl1';
+          DataStore.apiClientUrl = 'someUrl2';
+        });
+
+        it('returns false', function () {
+          expect(subject()).toBe(false);
+        });
+      });
+
+      describe('the message has the same origin as the api client url', function () {
+        beforeEach(function () {
+          message.origin = 'someUrl1';
+          DataStore.apiClientUrl = 'someUrl1';
+        });
+
+        it('returns true', function () {
+          expect(subject()).toBe(true);
+        });
+      });
+    });
+
+    describe('handle', function () {
+      var message;
+
+      beforeEach(function () {
+        message = {};
+        subject = function () {
+          instance.handle(message);
+        }
+      });
+
+      describe('the message is authorized', function () {
+        beforeEach(function () {
+          message.origin = 'someUrl1';
+          DataStore.apiClientUrl = 'someUrl1';
+        });
+
+        describe('the message has a serialized JSON object as its data', function () {
+          var options;
+          beforeEach(function () {
+            options = {};
+            subject = function () {
+              message.data = JSON.stringify(options);
+              return instance.handle(message);
+            }
+          });
+
+          describe('the action property of the options object is getUserAccessToken', function () {
+            beforeEach(function () {
+              options.action = 'getUserAccessToken';
+            });
+
+            it('posts a message to the source of the message with the user access token from the DataStore', function () {
+              DataStore.userAccessToken = 'someToken';
+              message.source = jasmine.createSpyObj('source', ['postMessage']);
+
+              subject();
+
+              expect(message.source.postMessage).toHaveBeenCalledWith('someToken');
+            });
+          });
+        });
+      });
+    });
+  });
+});
