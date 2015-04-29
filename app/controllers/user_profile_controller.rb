363c92678092a5d237b2e42f0b70eeed4767809e
adi <nistor.adrian@agilefreaks.com>
2015-04-29 14:41:50 +0300

    Changing the user plan, changes it in the API

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,5 +1,7 @@
 class UserProfileController < DashboardController
   def show
+    @current_api_user = OmniApi::User.where(email: current_user.email).first
+
     render
   end
 end
--- lib/omni_api/user.rb
+++ lib/omni_api/user.rb
@@ -2,6 +2,14 @@ module OmniApi
   class User < BaseClientModel
     include Concerns::Timestamps
 
-    attr_accessible :id, :first_name, :last_name, :access_token, :plan
+    attr_accessible :first_name, :last_name, :access_token, :plan
+
+    def self.change_plan!(email, plan)
+      headers['Content-Length'] = '0'
+      put plan.to_s, [], {email: email}.to_json
+      headers.delete('Content-Length')
+
+      return
+    end
   end
 end
