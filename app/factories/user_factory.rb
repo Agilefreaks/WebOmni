3c45fe697a25a6f715afb4fdddc9f1810a917c1f
adi <nistor.adrian@agilefreaks.com>
2015-04-30 10:51:35 +0300

    Adds flow for authenticating the access to Calendar API

--- app/factories/user_factory.rb
+++ app/factories/user_factory.rb
@@ -2,8 +2,6 @@ class UserFactory
   include Singleton
 
   def self.from_social(auth, user)
-    Track.sign_up(auth.info.email)
-
     UserFactory.instance.create_or_update_from_social(auth, user)
   end
 
@@ -13,7 +11,7 @@ class UserFactory
                OmniApi::User.new(first_name: auth.info.first_name,
                                  last_name: auth.info.last_name,
                                  email: auth.info.email.downcase,
-                                 image_url: get_image_url(auth))
+                                 image_url: auth.info.image)
     api_user.save
 
     params = {
@@ -21,7 +19,7 @@ class UserFactory
       last_name: auth.info.last_name,
       email: auth.info.email,
       password: Devise.friendly_token[0, 20],
-      image_url: get_image_url(auth),
+      image_url: auth.info.image,
       access_token: api_user.access_token,
       mixpanel_distinct_id: auth.distinct_id,
       remote_ip: auth.remote_ip
@@ -35,10 +33,5 @@ class UserFactory
     end
 
     user
-  end
-
-  def get_image_url(auth)
-    match = auth.to_s.match(/image="(.*?)"/)
-    match ? match[1] : nil
   end
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,3 +2,11 @@ Fabricator(:user) do
   password 'the art of war'
   password_confirmation 'the art of war'
 end
+
+Fabricator(:user_with_calendar_access, from: :user) do
+  email 'email@domain.com'
+  expires true
+  expires_at DateTime.now + 1.days
+  token 'token'
+  refresh_token 'refresh_token'
+end
