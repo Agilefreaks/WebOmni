a342967d5c4ac11cccb8bb654073396234ad058c
adi <nistor.adrian@agilefreaks.com>
2015-05-21 20:01:36 +0300

    Adds spec for syncing the user's calendars

--- app/services/calendars.rb
+++ app/services/calendars.rb
@@ -1,42 +1,40 @@
-module Google
-  class Calendars
-    def initialize(api_client)
-      @api_client = api_client
-    end
+class Calendars
+  def initialize(api_client)
+    @api_client = api_client
+  end
 
-    def list(user)
-      result = @api_client.execute(api_method: calendar_api.calendar_list.list, parameters: {}, authorization: credentials(user))
+  def list(user)
+    result = @api_client.execute(api_method: calendar_api.calendar_list.list, parameters: {}, authorization: credentials(user))
 
-      result.data['items']
-    end
+    result.data['items']
+  end
 
-    def watch(calendar)
-      result = @api_client.execute(
-        api_method: calendar_api.events.watch,
-        parameters: calendar.notification_channel.to_params,
-        authorization: credentials(calendar.user))
+  def watch(calendar)
+    result = @api_client.execute(
+      api_method: calendar_api.events.watch,
+      parameters: calendar.notification_channel.to_params,
+      authorization: credentials(calendar.user))
 
-      return result.status == 200
-    end
+    return result.status == 200
+  end
 
-    private
+  private
 
-    def credentials(user)
-      authorization = {
-        access_token: user.identity.token,
-        refresh_token: user.identity.refresh_token,
-        expires_at: user.identity.expires_at
-      }
+  def credentials(user)
+    authorization = {
+      access_token: user.identity.token,
+      refresh_token: user.identity.refresh_token,
+      expires_at: user.identity.expires_at
+    }
 
-      @authorization ||= (
-      auth = @api_client.authorization.dup
-      auth.update_token!(authorization)
-      auth
-      )
-    end
+    @authorization ||= (
+    auth = @api_client.authorization.dup
+    auth.update_token!(authorization)
+    auth
+    )
+  end
 
-    def calendar_api
-      @api_client.discovered_api('calendar', 'v3')
-    end
+  def calendar_api
+    @api_client.discovered_api('calendar', 'v3')
   end
 end
--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe CalendarsController do
-  describe '#show' do
-    subject { get :show }
+  describe '#index' do
+    subject { get :index, :format => "html" }
     context 'user signed in but doesnt have access to calendars api' do
-      let(:user) { Fabricate(:user_without_calendar_access) }
+      let(:user) { Fabricate(:user_without_calendar_access, identity: Fabricate(:expired_identity)) }
+
       before do
         allow(controller).to receive(:user_signed_in?).and_return(true)
         allow(controller).to receive(:current_user).and_return(user)
@@ -20,15 +21,52 @@ describe CalendarsController do
     end
 
     context 'user signed in and has access to calendars api' do
-      let(:user) { Fabricate(:user_with_calendar_access) }
+      let(:user) { Fabricate(:user_with_calendar_access, identity: Fabricate(:valid_identity)) }
+      let(:calendars_api) { double(Calendars) }
 
       before do
         allow(controller).to receive(:user_signed_in?).and_return(true)
         allow(controller).to receive(:current_user).and_return(user)
+        allow(calendars_api).to receive(:list)
+        allow(Calendars).to receive(:new).and_return(calendars_api)
       end
 
       it 'returns the path to show the calendars' do
-        expect(subject).to render_template(:show)
+        subject
+        expect(response).to render_template(:index)
+      end
+
+      context 'and there are new calendars' do
+        let(:remote_calendars) { [{ 'id' => '42', 'summary' => 'existing calendar' }, { 'id' => '43', 'summary' => 'new calendar' }] }
+        let(:existing_calendar) { Fabricate(:calendar, google_id: '42', summary: 'existing calendar') }
+
+        before do
+          user.calendars.push existing_calendar
+          allow(calendars_api).to receive(:list).and_return(remote_calendars)
+        end
+        it 'adds the new calendars to the user' do
+          expect { subject }.to change { user.calendars.count }.from(1).to(2)
+
+          expect(user.calendars.last.google_id).to eq '43'
+        end
+      end
+
+      context 'and there are calendars that were removed' do
+        let(:remote_calendars) { [{ 'id' => '42', 'summary' => 'existing calendar' }] }
+        let(:existing_calendar) { Fabricate(:calendar, google_id: '42', summary: 'existing calendar') }
+        let(:existing_calendar_to_be_removed) { Fabricate(:calendar, google_id: '43', summary: 'existing calendar to be removed') }
+
+        before do
+          user.calendars.push existing_calendar
+          user.calendars.push existing_calendar_to_be_removed
+          allow(calendars_api).to receive(:list).and_return(remote_calendars)
+        end
+
+        it 'removes the old calendars from the user' do
+          expect { subject }.to change { user.calendars.count }.from(2).to(1)
+
+          expect(user.calendars.last.google_id).to eq '42'
+        end
       end
     end
   end
