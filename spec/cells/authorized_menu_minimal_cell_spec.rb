810d61e3adbf740b0d2b0ef762789a7a60ff1fa2
Cristi Badila <cristi.badila@gmail.com>
2015-06-19 17:28:36 +0300

    [wo-382] adds headers for the embeddable views

---
+++ spec/cells/authorized_menu_minimal_cell_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe AuthorizedMenuMinimalCell, type: :cell do
+
+  context 'cell rendering' do
+  end
+
+end
---
+++ spec/cells/unauthorized_menu_minimal_cell_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe UnauthorizedMenuMinimalCell, type: :cell do
+
+  context 'cell rendering' do
+  end
+
+end
