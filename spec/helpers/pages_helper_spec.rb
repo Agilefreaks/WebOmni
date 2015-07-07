1954a2db3cff1104448eda1317e0c70cb6617f12
calin <calinoiu.alexandru@agilefreaks.com>
2015-07-07 20:13:44 +0300

    [oa-162] fixes client association creation

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -7,7 +7,7 @@ describe PagesHelper do
     context 'when user not signed in' do
       before do
         expect(helper).to receive(:user_signed_in?).twice.and_return(false)
-        expect(helper).to receive(:mobile_device?).and_return(true)
+        expect(helper).to receive(:is_mobile_device?).and_return(true)
       end
 
       it { is_expected.to eq user_omniauth_authorize_path(:google_oauth2, origin: android_client_downloads_path) }
