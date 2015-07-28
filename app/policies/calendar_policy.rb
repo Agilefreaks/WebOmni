518c399532fba0ab3aafe2478f8a2c4979ff5b07
calin <calinoiu.alexandru@agilefreaks.com>
2015-07-28 07:59:22 +0300

    [wo-387] temp fix for bad policy class

--- app/policies/calendar_policy.rb
+++ app/policies/calendar_policy.rb
@@ -2,7 +2,7 @@ class CalendarPolicy < ApplicationPolicy
   attr_reader :user, :calendar
 
   def access?
-    @user.plan.to_sym == :premium
+    @user.plan.to_sym == :premium if @user.plan
   end
 
   def show?
