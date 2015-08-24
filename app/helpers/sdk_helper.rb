62e85e200538f58c965ba898eec218bfd5ab7905
Cristi Badila <cristi.badila@gmail.com>
2015-08-24 17:50:13 +0300

    [#393] modifies the sdk controller and its show view so as to include i18n translations for the current client's culture

--- app/helpers/sdk_helper.rb
+++ app/helpers/sdk_helper.rb
@@ -1,2 +1,15 @@
 module SdkHelper
+  TRANSLATIONS_FILE_NAME = 'sdk.js'
+  DEFAULT_LOCALE = :en
+
+  def i18n_translations(locale)
+    locale = locale.to_sym
+    sdk_translations_segments = SimplesIdeias::I18n.translation_segments.select { |key, _| key.ends_with?(TRANSLATIONS_FILE_NAME) }
+    locale_translations_segment = sdk_translations_segments.find { |_, value| value.has_key?(locale) }
+    locale_translations_segment.try(:[], 1)
+  end
+
+  def i18n_translations_or_default(locale)
+    i18n_translations(locale) || i18n_translations(DEFAULT_LOCALE)
+  end
 end
